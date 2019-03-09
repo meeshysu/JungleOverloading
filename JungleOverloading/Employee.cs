02e86febdf887e2d7c626b0f91bb4fb1757c57b6
Michelle Beshears <meeshysu@gmail.com>
2019-03-09 13:44:59 -0600

    got the list of employees up

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Employee
+    {
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+
+        public List<Employee> EmployeeList = new List<Employee>();
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+
+
+    }
+}
